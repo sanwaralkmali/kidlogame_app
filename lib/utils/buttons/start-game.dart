fc187c81a059fa5903aad2e5945624fc3d9efb90
sanwaralkmali <mtnshsalah@gmail.com>
2023-07-11 12:01:10 +0300

    Show game dialoge and invite friend

--- lib/utils/buttons/start-game.dart
+++ lib/utils/buttons/start-game.dart
@@ -2,25 +2,31 @@
 
 import 'package:flutter/material.dart';
 import 'package:kidlogame_app/appTheme/themes.dart';
+import 'package:kidlogame_app/utils/showDialogs/push-play-game-dialog.dart';
 
 class StartGameButton extends StatelessWidget {
   const StartGameButton({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(8),
-        color: AppTheme.buttonColor,
-      ),
-      height: 35,
-      width: 120,
-      child: Center(
-        child: Text(
-          'Start Game',
-          style: TextStyle(
-            color: AppTheme.primaryTextColor,
-            fontSize: AppTheme.DEFAULT_TEXT_SIZE,
+    return GestureDetector(
+      onTap: () {
+        showGameSelectionDialog(context);
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: AppTheme.buttonColor,
+        ),
+        height: 35,
+        width: 120,
+        child: Center(
+          child: Text(
+            'Start Game',
+            style: TextStyle(
+              color: AppTheme.primaryTextColor,
+              fontSize: AppTheme.DEFAULT_TEXT_SIZE,
+            ),
           ),
         ),
       ),
